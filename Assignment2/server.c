59052d7d268d542f87e30cecc94805b9b7d944bb
Mandy Wong <manyumandy.wong@sjsu.edu>
2021-04-17 13:44:07 -0700

    Copy Assignment1 and re-exec itself after fork

---
+++ Assignment2/server.c
@@ -0,0 +1,112 @@
+// Server side C/C++ program to demonstrate Socket programming
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+
+#define PORT 8080
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[102] = {0};
+    char *hello = "Hello from server";
+
+    // When re-exec itself
+    if (getuid() != 0) {
+        // execvp will not end if we print any thing  
+        new_socket = atoi(argv[0]);
+        valread = read(new_socket, buffer, 1024);
+        // printf("%s\n",buffer);
+        send(new_socket, hello, strlen(hello), 0);
+        // printf("Hello message sent\n");
+        exit(0);
+        return 0;
+    }
+
+    printf("execve=0x%p\n", execve);
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Attaching socket to port 80
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                  &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+
+    printf("attached socket to port %d\n", PORT);
+
+    // Forcefully attaching socket to the port 80
+    if (bind(server_fd, (struct sockaddr *)&address,
+                                 sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                       (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t cid = fork();
+    if (cid == 0)
+    {
+        // Process under children process
+        // Drop privileges to "nobody" user
+        if (setuid(65534) != 0) {
+            perror("fail to drop privileges\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // Check if privileges dropped permanently
+        if (setuid(0) != -1) {
+            perror("privileges is able to restore\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // Store socket as string for argv
+        char socketStr[10];
+        sprintf(socketStr, "%d", new_socket);
+
+        // Re-exec itself
+        char *newArgv[2] = {socketStr, NULL};
+        execvp("./server", newArgv);
+
+        // After exec, the current process should be replaced
+        perror("fail to replace by exec\n");
+        exit(EXIT_FAILURE);
+    }
+    else if (cid < 0)
+    {
+        perror("fail to create child process\n");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        printf("children process created\n");
+    }
+
+    return 0;
+}
